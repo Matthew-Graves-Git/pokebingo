e0b8717616bb583fe78ca43c831d30c9120ff874
Matthew-Graves-Git <57644194+Matthew-Graves-Git@users.noreply.github.com>
2021-12-29 11:32:40 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+
+function loadS(){
+  let shield = require('shield.json');
+  console.log(shield);
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
+
+
